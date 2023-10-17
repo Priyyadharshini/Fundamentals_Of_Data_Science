f8372823c590bd2843d24048543040d5af99f7bd
Priyyadharshini <123918193+Priyyadharshini@users.noreply.github.com>
2023-10-17 09:38:50 +0530

    Add files via upload

---
+++ FODS_QN_08.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+# Sample data for monthly sales
+months = ['January', 'February', 'March', 'April', 'May']
+sales_values = [10000, 12000, 8000, 11000, 15000]
+
+# Creating a line plot for monthly sales data
+plt.figure(figsize=(8, 6))
+plt.plot(months, sales_values, marker='o', linestyle='--', color='b', label='Monthly Sales')
+plt.title('Monthly Sales Data')
+plt.xlabel('Months')
+plt.ylabel('Sales Values')
+plt.legend()
+plt.grid(True)
+
+# Creating a bar plot for monthly sales data
+plt.figure(figsize=(8, 6))
+plt.bar(months, sales_values, color='g', alpha=0.7)
+plt.title('Monthly Sales Data')
+plt.xlabel('Months')
+plt.ylabel('Sales Values')
+
+plt.show()
